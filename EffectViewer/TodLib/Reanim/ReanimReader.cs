d78fc39d20a6f84134ca9abfd9a7be535b883888
迎风听雨 <1259546233@qq.com>
2026-04-30 22:47:33 +0800

    删除doScale

--- EffectViewer/TodLib/Reanim/ReanimReader.cs
+++ EffectViewer/TodLib/Reanim/ReanimReader.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 
 namespace EffectViewer.TodLib.Reanim
@@ -29,50 +28,11 @@ namespace EffectViewer.TodLib.Reanim
         private static readonly DefMap<ReanimatorDefinition> ReanimatorDefMap = new(
             () => new ReanimatorDefinition(),
             DefinitionMapLoader.Array<ReanimatorDefinition, ReanimatorTrack>("track", ReanimatorTrackDefMap, static (ref ReanimatorDefinition reanim, ReanimatorTrack track) => AddTrack(ref reanim, track)),
-            DefinitionMapLoader.Float<ReanimatorDefinition>("fps", static (ref ReanimatorDefinition reanim, float value) => reanim.mFPS = value),
-            new DoScaleField());
+            DefinitionMapLoader.Float<ReanimatorDefinition>("fps", static (ref ReanimatorDefinition reanim, float value) => reanim.mFPS = value));
 
         public static ReanimatorDefinition Decode(Stream stream)
         {
-            ReanimLoadContext.ScaleType = ReanimScaleType.ScaleFromPC;
-            ReanimatorDefinition reanim = DefinitionMapLoader.Load(stream, ReanimatorDefMap);
-            ApplyScale(reanim, ReanimLoadContext.ScaleType);
-            ReanimLoadContext.ScaleType = ReanimScaleType.ScaleFromPC;
-            return reanim;
-        }
-
-        private static void ApplyScale(ReanimatorDefinition reanim, ReanimScaleType scaleType)
-        {
-            if (scaleType != ReanimScaleType.InvertAndScale || reanim.mTracks is null)
-            {
-                return;
-            }
-
-            for (int i = 0; i < reanim.mTrackCount; i++)
-            {
-                ReanimatorTrack track = reanim.mTracks[i];
-                bool isGround = ReanimatorXnaHelpers.ReanimatorTrackNameToId(track.mName) == Reanimation.ReanimTrackId__ground;
-                if (isGround || track.mTransforms is null)
-                {
-                    continue;
-                }
-
-                for (int j = 0; j < track.mTransformCount; j++)
-                {
-                    ReanimatorTransform transform = track.mTransforms[j];
-                    if (transform.mTransX != ReanimatorXnaHelpers.DEFAULT_FIELD_PLACEHOLDER)
-                    {
-                        transform.mTransX *= 1.875f;
-                    }
-
-                    if (transform.mTransY != ReanimatorXnaHelpers.DEFAULT_FIELD_PLACEHOLDER)
-                    {
-                        transform.mTransY *= 1.875f;
-                    }
-
-                    track.mTransforms[j] = transform;
-                }
-            }
+            return DefinitionMapLoader.Load(stream, ReanimatorDefMap);
         }
 
         private static void AddTrack(ref ReanimatorDefinition reanim, ReanimatorTrack track)
@@ -91,68 +51,6 @@ namespace EffectViewer.TodLib.Reanim
             transforms[^1] = transform;
             track.mTransforms = transforms;
             track.mTransformCount = (short)transforms.Length;
-        }
-
-        private sealed class DoScaleField : IDefField<ReanimatorDefinition>
-        {
-            public string Name => "doScale";
-            public DefFieldType FieldType => DefFieldType.Int;
-
-            public bool TryRead(SexyXmlParser parser, string elementName, ref ReanimatorDefinition definition)
-            {
-                if (!string.Equals(elementName, Name, StringComparison.OrdinalIgnoreCase))
-                {
-                    return false;
-                }
-
-                string value = ReadElementText(parser);
-                if (sbyte.TryParse(value, NumberStyles.Integer, CultureInfo.InvariantCulture, out sbyte parsed))
-                {
-                    ReanimLoadContext.ScaleType = (ReanimScaleType)(byte)parsed;
-                    return true;
-                }
-
-                throw parser.CreateError($"Can't parse int value '{value}'");
-            }
-
-            private static string ReadElementText(SexyXmlParser parser)
-            {
-                if (!parser.TryNextElement(out SexyXmlElement element))
-                {
-                    throw parser.CreateError("Missing element value");
-                }
-
-                if (element.Type == SexyXmlElementType.End)
-                {
-                    return string.Empty;
-                }
-
-                if (element.Type != SexyXmlElementType.Element)
-                {
-                    throw parser.CreateError("unknown element type");
-                }
-
-                string value = element.Value;
-                if (!parser.TryNextElement(out element) || element.Type != SexyXmlElementType.End)
-                {
-                    throw parser.CreateError("Missing element end");
-                }
-
-                return value;
-            }
-        }
-
-        private static class ReanimLoadContext
-        {
-            [ThreadStatic]
-            public static ReanimScaleType ScaleType;
-        }
-
-        internal enum ReanimScaleType
-        {
-            NoScale,
-            InvertAndScale,
-            ScaleFromPC = 0xFF
         }
     }
 }
